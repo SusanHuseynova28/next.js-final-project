8d4ff9f90e590546cdbffc3315645f6579b8ae38
unknown <susen.huseynova.11@mail.ru>
2024-08-28 20:53:04 +0400

    fix:login

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,15 @@
 'use client'
 import Register from "./register/page";
-import { useEffect } from "react";
 import { redirect } from 'next/navigation'
 
 
 export default function Home() {
-  // useEffect(()=>{
-  //   redirect("/register")
-  // },[])
+
   return (
   <main> 
-    
+    <Home/>
+    salam,
   </main>
   );
+
 }
